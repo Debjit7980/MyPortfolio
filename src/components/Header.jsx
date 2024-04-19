b177cb12acbfe93705dcb641d02ea57c72bb864c
Debjit7980 <62738111+Debjit7980@users.noreply.github.com>
2024-04-20 00:12:45 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header({ socialHandles }) {
                 <div className='socials flex gap-[20px] ml-auto'>
                     {/* Map over the social handles data and display only the image URLs */}
                     {socialHandles && socialHandles.map(handle => (
-                      <img key={handle._id} src={handle.image.url} alt={handle.platform} className='hover:scale-125 hover:duration-500 md:w-[28px] md:h-[28px] w-[22px] h-[22px]'/>
+                      <img key={handle._id} src={handle.image.url} alt={handle.platform} className='hover:scale-125 hover:duration-500 cursor-pointer md:w-[28px] md:h-[28px] w-[22px] h-[22px]'/>
                     ))}
                 </div>
             </div>
