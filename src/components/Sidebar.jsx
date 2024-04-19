70faa8a7cdbe203501ca249f2a32fcaeda648868
Debjit7980 <62738111+Debjit7980@users.noreply.github.com>
2024-04-20 01:16:17 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-scroll'
 import {delay, motion} from 'framer-motion';
 import { RiMenuUnfoldLine } from "react-icons/ri";
 import { RiMenuFoldLine } from "react-icons/ri";
@@ -59,9 +58,9 @@ function Sidebar() {
         <motion.div className='fixed z-40 top-0 left-0 bottom-0 w-[400px] bg-white' variants={variant}>
             <motion.div className='absolute w-[100%] h-screen flex flex-col items-center justify-center gap-[30px] text-2xl tracking-wider' variants={linkVariants}>
                 {links.map((link)=>(
-                    <motion.Link to={`${link}`} spy={true} smooth={true} offset={50} duration={500} key={link} variants={itemVariants} onClick={()=>setOpen((prev)=>!prev)} whileHover={{scale:1.2}}  whileTap={{scale:0.95}}>
+                    <motion.a href={`#${link}`} key={link} variants={itemVariants} onClick={()=>setOpen((prev)=>!prev)} whileHover={{scale:1.2}}  whileTap={{scale:0.95}}>
                         {link}
-                    </motion.Link>
+                    </motion.a>
                 ))}
             </motion.div>
         </motion.div>
